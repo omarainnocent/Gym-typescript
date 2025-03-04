2f62eaf9b1c244b3be24705a08fe3ef189cffc36
im <omarainnocent644@gmail.com>
2025-03-04 14:56:29 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,5 @@
 import Navbar from "@/scenes/navbar";
-import Home from "@/scenes/home";
-import OurClasses from "@/scenes/ourClasses";
-import Benefits from "@/scenes/benefits";
-import ContactUs from "@/scenes/contactUs";
-import Footer from "@/scenes/footer";
+
 import { useEffect, useState } from "react";
 import { SelectedPage } from "@/shared/types";
 
@@ -32,11 +28,7 @@ function App() {
         selectedPage={selectedPage}
         setSelectedPage={setSelectedPage}
       />
-      <Home setSelectedPage={setSelectedPage} />
-      <Benefits setSelectedPage={setSelectedPage} />
-      <OurClasses setSelectedPage={setSelectedPage} />
-      <ContactUs setSelectedPage={setSelectedPage} />
-      <Footer />
+  
     </div>
   );
 }
